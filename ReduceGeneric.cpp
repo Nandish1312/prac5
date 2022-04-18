0564f460dd5ca5dd3eb27fbeafe9116b55862480
Nandish1312 <76993210+Nandish1312@users.noreply.github.com>
2022-04-18 18:51:29 +0930

    Add files via upload

--- ReduceGeneric.cpp
+++ ReduceGeneric.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
 #include "ReduceGeneric.h"
 using namespace std;
 
 
-int ReduceGeneric::reduce(vector<int> array) {
-  if (array.size() == 2) {
-    return binaryOperator(array[0], array[1]);
+int ReduceGeneric::reduce(vector<int> reduce) 
+
+{
+  
+  if (reduce.size() == 2) 
+  
+  {
+    
+    return binaryOperator(reduce[0], reduce[1]);
+    
   }
-  vector <int> temp(array.begin(), array.end()-1);
-  return binaryOperator(reduce(temp),array[array.size()-1]);
+  
+  vector <int> num(reduce.begin(), reduce.end()-1);
+  
+  return binaryOperator(reduce(num),reduce[reduce.size()-1]);
 }
+
+
+
